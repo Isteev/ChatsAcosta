ade05510b0d57d9578ccabdbcc0e6d58e704d124
steveen de la rosa <elisra0412@gmail.com>
2023-09-11 23:30:20 -0500

    cron inactive

--- src/components/meetings/meetingsRoute.js
+++ src/components/meetings/meetingsRoute.js
@@ -4,7 +4,7 @@ import meetignController from "./meetingsController.js";
 const meetingRoute = new Router();
 
 meetingRoute.post("/", meetignController.addAction);
-meetingRoute.get("/inactive", meetignController.InactiveCron);
+meetingRoute.get("/cron/inactive", meetignController.InactiveCron);
 meetingRoute.get("/:channel_id", meetignController.getByChannel);
 
 export default meetingRoute;
